2491bbb8e9337d573b7106b9a2aa469d399b19b2
Evariste Nkurunziza <nkurunzizaevariste060@gmail.com>
2025-09-23 20:48:03 +0200

    add a profile section on the page

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,11 @@ export default {
   content: ["./index.html"
     ,"./src**/*.{js,ts,jsx,tsx}",],
   theme: {
-    extend: {},
+    extend: {
+      boxShadow: {
+        'eva':"0 0 10px 10px rgba(0,100,200,0.35)",
+      },
+    },
   },
   plugins: [],
 }
