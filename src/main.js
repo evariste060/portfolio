f6624d0dba3dab2a62b2c12db9f9db542668537e
Evariste Nkurunziza <nkurunzizaevariste060@gmail.com>
2025-09-24 20:25:51 +0200

    add the feedback section on the page

--- src/main.js
+++ src/main.js
@@ -11,4 +11,20 @@ close.addEventListener('click',()=>{
     close.classList.toggle('hidden');
     menu.classList.toggle('hidden');
 })
-console.log(close)
+function ReplyFeedback(){
+    let name = document.getElementById('name').value
+    let email = document.getElementById('email').value
+    let feedback = document.getElementById('feedback').value
+    let response = document.getElementById('response');
+    response.innerHTML = `
+    <h1 class="text-2xl font-bold text-red-600">Thank you for giving us a feedback<h1>
+    Hello ${name} 
+    Your feeback ${feedback} 
+    Is warmly well received and we thank you for your participation for our hardwork
+    toword success
+
+    
+    ` 
+}
+let button = document.getElementById('submit')
+button.addEventListener('click',ReplyFeedback)
